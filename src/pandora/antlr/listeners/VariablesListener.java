7c2e1d032a9b4c4a18897ab310cc0afab3af5ddb
Gabriel Luz <me@gabrielluz.com>
2017-10-24 14:10:24 -0200

    add stack of variables map

--- src/pandora/antlr/listeners/VariablesListener.java
+++ src/pandora/antlr/listeners/VariablesListener.java
@@ -5,23 +5,32 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import src.pandora.antlr.PandoraBaseListener;
 import src.pandora.antlr.PandoraParser;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class VariablesListener extends PandoraBaseListener {
-    private Map<String, Variable> variables;
-
+    private Stack blockStack;
     public VariablesListener() {
-        variables = new HashMap<String, Variable>();
+        blockStack = new Stack();
+    }
+
+    @Override
+    public void enterBlock(PandoraParser.BlockContext ctx) {
+        //Something starts putting definitions on hash
+//        System.out.println("ctx.getParent(). = " + ctx.getParent().);
+        blockStack.push(new HashMap<String, Variable>());
+
+    }
+    @Override
+    public void exitBlock(PandoraParser.BlockContext ctx) {
+        //Something pops the definitions from hash
+        blockStack.pop();
     }
 
     @Override
     public void enterDefinition(PandoraParser.DefinitionContext ctx) {
         ctx.namelist().NAME().forEach(nameToken -> {
-            if (variables.get(nameToken.getText()) != null) {
+            if (((HashMap<String,Variable>) blockStack.peek()).containsKey(nameToken.getText())) {
                 throw new RuntimeException("Variable <" + nameToken.getText() + "> already was defined");
             }
         });
@@ -38,14 +47,14 @@ public class VariablesListener extends PandoraBaseListener {
                     TerminalNode nameToken = nameList.get(idx);
                     PandoraParser.ExpContext exp = expList.get(idx);
 
-                    variables.put(nameToken.getText(), new Variable(exp));
+                    ((HashMap<String,Variable>) blockStack.peek()).put(nameToken.getText(), new Variable(exp));
                 });
     }
 
     @Override
     public void enterAssignment(PandoraParser.AssignmentContext ctx) {
         ctx.namelist().NAME().forEach(nameToken -> {
-            if (variables.get(nameToken.getText()) == null) {
+            if (!((HashMap<String,Variable>) blockStack.peek()).containsKey(nameToken.getText())) {
                 throw new RuntimeException("Variable <" + nameToken.getText() + "> ins't defined");
             }
         });
@@ -61,8 +70,7 @@ public class VariablesListener extends PandoraBaseListener {
                 .forEach(idx -> {
                     TerminalNode nameToken = nameList.get(idx);
                     PandoraParser.ExpContext exp = expList.get(idx);
-
-                    variables.put(nameToken.getText(), new Variable(exp));
+                    ((HashMap<String,Variable>) blockStack.peek()).put(nameToken.getText(), new Variable(exp));
                 });
     }
 }
